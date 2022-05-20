913f9a9191b2808064c9d746428b13d3d313a1f3
darahgarcia <daraheglopes@gmail.com>
2022-05-20 17:34:38 -0400

    Aprender

--- tarefa-dia3/pratica1.js
+++ tarefa-dia3/pratica1.js
@@ -3,3 +3,9 @@ var email = "darahgarcia@hotmail.com";
 var profissao = 'engenheira';
 var idade = 20;
 var lugarFav = 'Grecia';
+
+console.log(nome);
+console.log(email);
+console.log(profissao);
+console.log(lugarFav);
+console.log(idade);
